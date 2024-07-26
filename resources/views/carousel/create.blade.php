12bdd31bc6cf73853441fd79b8fa9fd7a223fb4c
Juan Gerardo Alcantar Torres <53959987+GDesasT@users.noreply.github.com>
2024-07-25 18:19:42 -0600

    Update create.blade.php

--- resources/views/carousel/create.blade.php
+++ resources/views/carousel/create.blade.php
@@ -3,17 +3,36 @@
 @section('content')
 <div class="container mx-auto mt-5">
     <h1 class="text-3xl font-bold mb-4">Agregar Nueva Imagen</h1>
+    @if (session('success'))
+        <div class="bg-green-500 text-white p-4 rounded mb-4">
+            {{ session('success') }}
+        </div>
+    @endif
+    @if ($errors->any())
+        <div class="bg-red-500 text-white p-4 rounded mb-4">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     <form action="{{ route('carousel.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
         <div class="mb-4">
-            <label for="image" class="block text-gray-700">Imagen:</label>
-            <input type="file" name="image" id="image" class="mt-1 block w-full" required>
-        </div>
-        <div class="mb-4">
-            <label for="active" class="inline-flex items-center">
-                <input type="checkbox" name="active" id="active" class="form-checkbox">
-                <span class="ml-2 text-gray-700">Activo</span>
-            </label>
+            <label for="dropzone-file" class="block text-gray-700">Imagen:</label>
+            <div class="flex items-center justify-center w-full">
+                <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
+                    <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                        <svg class="w-8 h-8 mb-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                        </svg>
+                        <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+                        <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                    </div>
+                    <input id="dropzone-file" type="file" name="image" class="hidden" required>
+                </label>
+            </div>
         </div>
         <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded">Guardar Imagen</button>
     </form>
