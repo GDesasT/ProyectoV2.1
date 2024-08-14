3dfc1dbb860f3258494575e4c8a6f05c03d6157a
Juan Gerardo Alcantar Torres <53959987+GDesasT@users.noreply.github.com>
2024-08-14 14:10:01 -0600

    navs

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,8 +1,8 @@
 <nav class="bg-blue-300 rounded-b-2xl border-gray-200" x-data="{ open: false }">
-    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="{{ route('home') }}" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="{{ asset('img/logo.jpeg') }}" class="h-12 rounded-full" alt="Comedor Logo" />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">Comedores De la Fuente</span>
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2 md:p-4">
+        <a href="{{ route('home') }}" class="flex items-center space-x-2 md:space-x-3 rtl:space-x-reverse">
+            <img src="{{ asset('img/logo.jpeg') }}" class="h-10 md:h-12 rounded-full" alt="Comedor Logo" />
+            <span class="self-center text-lg md:text-xl font-semibold whitespace-nowrap text-white">Comedores De la Fuente</span>
         </a>
         <button @click="open = !open" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-400 rounded-lg md:hidden hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
             <span class="sr-only">Open main menu</span>
@@ -11,7 +11,7 @@
             </svg>
         </button>
         <div :class="{ 'block': open, 'hidden': !open }" class="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-700 rounded-lg bg-blue-300 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+            <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-700 rounded-lg bg-blue-300 md:flex-row md:space-x-6 md:mt-0 md:border-0">
                 <li>
                     <a href="{{ route('home') }}" class="block py-2 px-3 rounded {{ request()->routeIs('home') ? 'text-white bg-blue-200 md:bg-transparent md:text-blue-500' : 'text-white hover:bg-gray-400 md:hover:bg-transparent md:border-0 md:hover:text-blue-500' }}" aria-current="{{ request()->routeIs('home') ? 'page' : '' }}">Home</a>
                 </li>
@@ -23,14 +23,8 @@
                         class="block py-2 px-3 rounded {{ request()->routeIs('inventory') ? 'text-white bg-blue-200 md:bg-transparent md:text-blue-500' : 'text-white hover:bg-gray-400 md:hover:bg-transparent md:border-0 md:hover:text-blue-500' }}">
                         {{ auth()->check() ? 'Inventario' : 'Login' }}
                      </a>
-                     
                 </li>
             </ul>
         </div>
     </div>
 </nav>
-
-<script>
-
-    
-</script>
