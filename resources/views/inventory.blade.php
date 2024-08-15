2def91b3edb1446ee3722ea0eccbcd1c95bb2ad4
Juan Gerardo Alcantar Torres <53959987+GDesasT@users.noreply.github.com>
2024-08-14 18:14:58 -0600

    Update inventory.blade.php

--- resources/views/inventory.blade.php
+++ resources/views/inventory.blade.php
@@ -54,7 +54,6 @@
                         <option value="Proteina">Proteina</option>
                         <option value="Cereales y Legumbres">Cereales y Legumbres</option>
                     </select>
-                       
                         <div class="tooltip-light hidden text-center group-hover:block absolute z-10 w-64 px-3 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg shadow-sm">
                             Buscar por categoria
                             <div class="tooltip-arrow" data-popper-arrow></div>
@@ -91,75 +90,78 @@
         </div>
 
         <!-- Modal para agregar producto -->
-        <div id="crud-modal" tabindex="-1" aria-hidden="true"
-            class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-            <div class="relative p-4 w-full max-w-md max-h-full">
-                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                    <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                            Agregar producto / Añadir
-                        </h3>
-                        <button onclick="closeModal()" type="button"
-                            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                            data-modal-toggle="crud-modal">
-                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                viewBox="0 0 14 14">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                            </svg>
-                            <span class="sr-only">Close modal</span>
-                        </button>
-                    </div>
-                    <form id="product-form" class="p-4 md:p-5" method="POST" action="{{ route('inventory.store') }}">
-                        @csrf
-                        <div class="grid gap-4 mb-4 grid-cols-2">
-                            <div class="col-span-2">
-                                <label for="name" class="block mb-2 text-sm text-center font-medium text-gray-900 dark:text-white">Nombre</label>
-                                <input type="text" name="name" id="name"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                    placeholder="Nombre del producto" required>
-                            </div>
+<div id="crud-modal" tabindex="-1" aria-hidden="true"
+class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+<div class="relative p-4 w-full max-w-md max-h-full">
+    <div class="relative bg-white rounded-lg shadow">
+        <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
+            <h3 class="text-lg font-semibold text-gray-900">
+                Agregar producto / Añadir
+            </h3>
+            <button onclick="closeModal()" type="button"
+                class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
+                data-modal-toggle="crud-modal">
+                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                </svg>
+                <span class="sr-only">Close modal</span>
+            </button>
+        </div>
+        <form id="product-form" class="p-4 md:p-5" method="POST" action="{{ route('inventory.store') }}">
+            @csrf
+            <div class="grid gap-4 mb-4 grid-cols-2">
+                <div class="col-span-2">
+                    <label for="name" class="block mb-2 text-sm text-center font-medium text-gray-900">Nombre</label>
+                    <input type="text" name="name" id="name"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                        placeholder="Nombre del producto" required>
+                </div>
+            
+                <div class="col-span-2 sm:col-span-1">
+                    <label for="amount" class="block mb-2 text-sm text-center font-medium text-gray-900">Cantidad</label>
+                    <input type="number" name="amount" id="amount" step="0.01"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                        placeholder="" required>
+                </div>                            
                     
-                            <div class="col-span-2 sm:col-span-1">
-                                <label for="amount" class="block mb-2 text-sm text-center font-medium text-gray-900 dark:text-white">Cantidad</label>
-                                <input type="number" name="amount" id="amount"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                    placeholder="" required>
-                                
-                                <select id="unit" name="unit"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
-                                    <option selected disabled value="">Seleccionar unidad</option>
-                                    <option value="Kg">Kilogramos(Kg)</option>
-                                    <option value="L">Litros(L)</option>
-                                    <option value="Pz">Unidades(Pz)</option>
-                                </select>
-                            </div>
-                    
-                            <div class="col-span-2 sm:col-span-1">
-                                <label for="type" class="block mb-2 text-sm text-center font-medium text-gray-900 dark:text-white">Categoría</label>
-                                <select id="type" name="type"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required>
-                                    <option selected disabled value="">Seleccionar Categoría</option>
-                                    <option value="Verdura">Verdura</option>
-                                    <option value="Fruta">Fruta</option>
-                                    <option value="Proteina">Proteina</option>
-                                    <option value="Cereales y Legumbres">Cereales y Legumbres</option>
-                                </select>
-                            </div>
-                        </div>
-                    
-                        <button type="submit"
-                            class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path>
-                            </svg>
-                            Agregar producto / Añadir
-                        </button>
-                    </form>
-                    
+                <div class="col-span-2 sm:col-span-1">
+                    <label for="unit" class="block mb-2 text-sm text-center font-medium text-gray-900">Unidad</label>
+                    <select id="unit" name="unit"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" required>
+                        <option selected disabled value="">Seleccionar unidad</option>
+                        <option value="Kg">Kilogramos(Kg)</option>
+                        <option value="L">Litros(L)</option>
+                        <option value="Pz">Unidades(Pz)</option>
+                    </select>
+                </div>
+            
+                <div class="col-span-2">
+                    <label for="type" class="block mb-2 text-sm text-center font-medium text-gray-900">Categoría</label>
+                    <select id="type" name="type"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" required>
+                        <option selected disabled value="">Seleccionar Categoría</option>
+                        <option value="Verdura">Verdura</option>
+                        <option value="Fruta">Fruta</option>
+                        <option value="Proteina">Proteina</option>
+                        <option value="Cereales y Legumbres">Cereales y Legumbres</option>
+                    </select>
                 </div>
             </div>
-        </div>
+
+            <div class="col-span-2">
+                <button type="submit"
+                    class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                    Agregar producto / Añadir
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
+</div>
+
+
 
         <br>
         <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
@@ -175,7 +177,7 @@
                 </thead>
                 <tbody>
                     @foreach ($inventories as $inventory)
-                        <tr class="bg-white border-b dark:hover:bg-gray-100">
+                        <tr class="bg-white border-b">
                             <td class="px-6 py-4">{{ $inventory->name}}</td>
                             <td class="px-6 py-4">{{ $inventory->amount }} {{ $inventory->unit}}
                             <td class="px-6 py-4">{{ $inventory->type }}</td>
